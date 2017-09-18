5318d4d764e68e2aee622c3cb795ba14e4300e2e
SETH PERKINS <seth.perkins@gmail.com>
2017-09-18 12:07:02 -0400

    This is still ridiculously hackish, but the basic idea of SpriteLayer2, is that the texture is being generated outside the SpriteLayer and handed to the module.  Layers own drawing - Texture generating/mgt is something else

--- src/Layers/SpriteLayer2.js
+++ src/Layers/SpriteLayer2.js
@@ -10,10 +10,7 @@ function SpriteLayer(parent, options) {
     if(options === undefined) return console.error("Invalid options provided to SpriteLayer");
     // TODO: Better error handling here
 
-    if(options.spriteName === undefined) return console.error("spriteName must be provided to SpriteLayer");
-    if(options.spritesheetPath === undefined) return console.error("spritesheetPath must be provided to SpriteLayer");
-    if(options.offsetX === undefined) return console.error("offsetX must be provided to SpriteLayer");
-    if(options.offsetY === undefined) return console.error("offsetY must be provided to SpriteLayer");
+    if(options.texture === undefined) return console.error("texture must be provided to SpriteLayer");
     if(options.scale === undefined) options.scale = 1.0;
     if(options.scaleWithMapProjection === undefined) options.scaleWithMapProjection = true;
     if(options.key === undefined || options.key === '' || (typeof options.key === 'object' && options.key.property === undefined && options.key.generator === undefined)) {
@@ -30,10 +27,7 @@ function SpriteLayer(parent, options) {
     else
         this.source = options.source;
 
-    this.spritesheetPathGen = ValueGenerator(options.spritesheetPath);
-    this.spriteNameGen = ValueGenerator(options.spriteName);
-    this.spriteNameGen = ValueGenerator(options.offsetX);
-    this.spriteNameGen = ValueGenerator(options.offsetY);
+    this.texture = options.texture;
     this.scaleGen = ValueGenerator(options.scale);
     this.features = Object.create(null);
     this.scaleWithMapProjection = options.scaleWithMapProjection;
@@ -44,10 +38,10 @@ function SpriteLayer(parent, options) {
 
 SpriteLayer.prototype = {
     updateSourceData: function(source, absolute) {
-        var oldFeatures = {}
+//        var oldFeatures = {}
 
         if (!source.features) return console.error("updateSourceData expects a GeoJSON FeatureCollection with a 'features' property");
-        source.features.forEach((feature, i) => {
+/*       source.features.forEach((feature, i) => {
             const key = this.keyGen(feature,i); // TODO: error handling
             if (key in this.features) {
                 // Update
@@ -60,14 +54,13 @@ SpriteLayer.prototype = {
                     geojson: feature
                 };
                 this.features[key].properties = {
-                    spritesheetPath: spritesheetPath,
-                    spriteName: spriteName
                 };
             }
         });
-
-        this._addOrUpdateFeatures(this.features)
-
+*/
+        this.source = source;
+        this._addOrUpdateFeatures(this.source.features, this.texture);
+/*
         if(absolute) {
             // Check for any features that are not have not been updated and remove them from the scene
             for(key in this.features) {
@@ -76,8 +69,8 @@ SpriteLayer.prototype = {
                 }
             }
         }
-
-        this.source = source;
+*/
+ //       this.source = source;
 
     },
     removeFeature: function(key) {
@@ -87,47 +80,24 @@ SpriteLayer.prototype = {
     _initialize: function() {
  //       var spriteNames = [];
         
-                // Determine how to load the models
-                if(!this.spriteNameGen)
-                    return console.error("Invalid img name definition provided to SpriteLayer");
-                if(!this.spritesheetPathGen)
-                    return console.error("Invalid model directory definition provided to SpriteLayer");
-        
                 // Add features to a map
-                this.source.features.forEach((f,i) => {
-                    const key = this.keyGen(f,i); // TODO: error handling
-                    if(this.features[key] !== undefined) console.warn("Features with duplicate key: " + key);
-        
-                    const spritesheetPath = this.spritesheetPathGen(f,i);
-                    const spriteName = this.spriteNameGen(f,i);
-                    this.features[key] = {
-                        geojson: f,
-                        spritesheetPath: spritesheetPath,
-                        spriteName: spriteName                      
-                    }
-        
- //                  spriteNames.push({directory: spritesheetPath, name: spriteName});
-                });
-                this._addOrUpdateFeatures(this.features);
+                this._addOrUpdateFeatures(this.source.features, this.texture);
  
     },
-    _addOrUpdateFeatures: function(features) {
-        console.log("Entered spriteLayer._addOrUpdateFeatures with features = " + JSON.stringify(features) );
+    _addOrUpdateFeatures: function(features, _texture) {
+        console.log("Entered spriteLayer._addOrUpdateFeatures with _texture = " + _texture + " and features = " + JSON.stringify(features) );
         for (key in features) {
             const f = features[key];
-            const position = f.geojson.geometry.coordinates;
-            const scale = this.scaleGen(f.geojson);
-            const spritesheetPath = f.spritesheetPath;
- //           const rotation = this.rotationGen(f.geojson);
+            console.log("f = " + JSON.stringify(f));
+            const position = f.geometry.coordinates;
+            const scale = 100;
 
-            var spriteMap = new THREE.TextureLoader().load( spritesheetPath );
-            spriteMap.offset.x = f.offsetX;
-            spriteMap.offset.y = f.offsetY;
-            var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
+            var spriteMaterial = new THREE.SpriteMaterial( { map: _texture, color: 0xffffff } );
             var sprite = new THREE.Sprite( spriteMaterial );
 
             if (!f.rawObject) {
                 // Need to create a scene graph object and add it to the scene
+                console.log("adding Sprite, sprite = " + sprite + ", position = " + position + ", scale = " + scale );
                 this.parent.addAtCoordinate(sprite, position, {scaleToLatitude: this.scaleWithMapProjection, preScale: scale});
                 //this.features[key] = f;
             }
